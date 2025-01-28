a61e259bf28416b482fa2ebf87754848171a5e53
Daniel González <dgonzalez@iessarenal.net>
2025-01-28 08:50:30 +0100

    shit

--- exercises/librarygood.py
+++ exercises/librarygood.py
@@ -27,10 +27,39 @@ class DigitalBook(Book):
         # available = 'Available' if self.status else 'Borrowed'
         print(f'[Digital] Title: {self.title}\nAuthor: {self.author}\nStatus: always available')
 
+class BorrowableInterface(ABC):
+    @abstractmethod
+    def borrow(self):
+        pass
+
+    @abstractmethod
+    def return_book(self):
+        pass
+
+class BorrowablePrintedBook(PrintedBook, BorrowableInterface):
+    def borrow(self):
+        if self.status == 'Available':
+            self.status = 'Borrowed'
+            print(f'{self.title} borrowed successfully')
+        else:
+            print(f'{self.title} is already borrowed')
+    
+    def return_book(self):
+        self.status = 'Available'
+        print(f'{self.title} returned successfully')
+
+class ReportInterface(ABC):
+    @abstractmethod
+    def generate(self, books):
+        pass        
+
 class Library():
     def __init__(self, report_generator):
         self.books = []
-        self.report_generator = report_generator
+        self.report_generator = {}
+    
+    def add_generator(self, name, reportGenerator):
+        self.report_generator[name] = reportGenerator
     
     def add_book(self, book):
         self.books.append(book)
@@ -40,34 +69,49 @@ class Library():
             book.print_info()
     
     def borrow_book(self, book):
-        for b in self.books:
-            if b.title == book.title and b.status == 'Available':
-                if b.status:
-                    b.status = 'Borrowed'
-                    print(f'{b.title} borrowed successfully')
-                    break
-        else: # Only executes when entire loop is completed without break
-            print(f'{book.title} is already borrowed or is not in the library')
+        if isinstance(book, BorrowableInterface):
+            book.borrow()
+        else:
+            print(f'{book.title} is not borrowable')
+        
+        # for b in self.books:
+        #     if b.title == book.title and b.status == 'Available':
+        #         if b.status:
+        #             b.status = 'Borrowed'
+        #             print(f'{b.title} borrowed successfully')
+        #             break
+        # else: # Only executes when entire loop is completed without break
+        #     print(f'{book.title} is already borrowed or is not in the library')
 
-    def generate_report(self):
-        self.report_generator.generate(self.books)    
+    def return_book(self, book):
+        if isinstance(book, BorrowableInterface):
+            book.return_book()
+        else:
+            print(f'{book.title} can not be returned')
 
-class ReportInterface(ABC):
-    @abstractmethod
-    def generate(self, books):
-        pass
-
+    def generate_report(self, name):
+        self.report_generator[name].generate(self.books)    
+        
 class TextReport(ReportInterface):
     def generate(self, books):
         print("Book's report:")
         for book in books:
             book.print_info()
 
-book_1 = PrintedBook('The Hobbit', 'J.R.R. Tolkien', 'Available')
+book_1 = BorrowablePrintedBook('The Hobbit', 'J.R.R. Tolkien', 'Available')
 book_2 = DigitalBook('The Lord of Rings', 'J.R.R. Tolkien')
+book_3 = BorrowablePrintedBook('The Silmarillion', 'J.R.R. Tolkien', 'Available')
+
 
 my_library = Library(TextReport())
 
 my_library.add_book(book_1)
 my_library.add_book(book_2)
-my_library.generate_report()
+my_library.add_book(book_3)
+
+my_library.generate_report('CSV')
+
+my_library.borrow_book(book_1)
+my_library.borrow_book(book_2)
+
+my_library.generate_report('JSON')
